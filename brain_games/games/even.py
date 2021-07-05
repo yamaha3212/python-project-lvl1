a930843a456f1e6dfc4ae8f309c8175b990ed78c
yamaha3212 <yamaha3212@yandex.ru>
2021-07-05 16:41:49 +0300

    dev debug

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,11 +1,28 @@
-from random import randint
+import random
+from brain_games.cli
 
 
-def even_game():
-    ask_num = randint(1, 100)
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    print("Question: {}".format(ask_num))
-    prompt.string('Your answer: ')
+def main():
+    i = 1
+    while i <= 3:
+        num = random.randint(1, 100)
+        correct_answer = 'no'
+        if num % 2 == 0:
+            correct_answer = 'yes'
+        print("Question: {}".format(num))
+        reply = input('Your answer: ')
+
+        if reply == correct_answer:
+            print("Correct!")
+
+        else:
+            print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(
+                reply, correct_answer))
+            print("Let's try again, {}!".format(brain_games.cli.get_name))
+            return
+        i = i + 1
+    print('Congratulations, {}!'.format(brain_games.cli.get_name))
 
 
-even_game()
+if __name__ == "__main__":
+    main()
